488c9212ce222ab3c05ce2eb542d43cd8b103563
MattLawson98 <matthewlawson50@gmail.com>
2022-03-21 16:25:52 -0700

    This file helps create the template for tinyApp!

--- express_server.js
+++ express_server.js
@@ -4,10 +4,17 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  return Math.random().toString(25).slice(-6)
+}
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -15,6 +22,15 @@ app.get("/", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls.json", (req, res) => {

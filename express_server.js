8497097cb17a5257f3ff1afbffc91df1cbff6574
MattLawson98 <matthewlawson50@gmail.com>
2022-03-23 15:19:38 -0700

    This file helps create the tinyApp!

--- express_server.js
+++ express_server.js
@@ -5,7 +5,12 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+const cookies = require("cookie-parser")
+
+app.use(bodyParser.urlencoded({ extended: true }), cookies());
+
+
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -13,7 +18,7 @@ const urlDatabase = {
 };
 
 function generateRandomString() {
-  return Math.random().toString(25).slice(-6)
+  return Math.random().toString(25).slice(-6);
 }
 
 app.get("/", (req, res) => {
@@ -43,17 +48,29 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = {
+    username: req.cookies["username"], 
+    urls: urlDatabase
+  }; 
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  const templateVars = {
+    username: req.cookies["username"],
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL]
+  }
+  res.render("urls_index", templateVars); 
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  const templateVars = {
+   longURL : urlDatabase[req.params.shortURL],
+   username: req.cookies["username"],
+  }
+  res.render("urls_index", templateVars);
   res.redirect(longURL);
 });
 
@@ -61,6 +78,22 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 const shortURL = req.params.shortURL;
 delete urlDatabase[shortURL];
 res.redirect('/urls');
+});
+
+app.post("/login", (req, res) => {
+  res.cookie('username', `${req.body.username}`); 
+  res.redirect('/urls');
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
+
+app.post("/urls/:id", (req, res) => { 
+  const shortURL = req.params.id;
+  urlDatabase[shortURL].longURL = req.body.newURL;
+  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
